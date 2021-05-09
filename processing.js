cd4c0f8e35eafffcb931637fe12e3062f42f7d21
srinivas.kool <31861607+srinivaskool@users.noreply.github.com>
2021-05-09 17:24:22 +0530

    Update processing.js

--- processing.js
+++ processing.js
@@ -1,104 +1,323 @@
 var model;
 
 async function loadModel() {
-    model = await tf.loadGraphModel('TFJS/model.json')
+  model = await tf.loadGraphModel("TFJS/model.json");
 }
 
-function predictImage() {
-    // console.log('processing ...');
+function predictImage1() {
+  let image = cv.imread(my - canvas1);
+  cv.cvtColor(image, image, cv.COLOR_RGBA2GRAY, 0);
+  cv.threshold(image, image, 175, 255, cv.THRESH_BINARY);
 
-    let image = cv.imread(canvas);
-    cv.cvtColor(image, image, cv.COLOR_RGBA2GRAY, 0);
-    cv.threshold(image, image, 175, 255, cv.THRESH_BINARY);
+  let contours = new cv.MatVector();
+  let hierarchy = new cv.Mat();
 
-    let contours = new cv.MatVector();
-    let hierarchy = new cv.Mat();
-    // You can try more different parameters
-    cv.findContours(image, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+  cv.findContours(
+    image,
+    contours,
+    hierarchy,
+    cv.RETR_CCOMP,
+    cv.CHAIN_APPROX_SIMPLE
+  );
 
-    let cnt = contours.get(0);
-    let rect = cv.boundingRect(cnt);
-    image = image.roi(rect);
+  let cnt = contours.get(0);
+  let rect = cv.boundingRect(cnt);
+  image = image.roi(rect);
 
-    var height = image.rows;
-    var width = image.cols;
+  var height = image.rows;
+  var width = image.cols;
 
-    if (height > width) {
-        height = 20;
-        const scaleFactor = image.rows / height;
-        width = Math.round(image.cols / scaleFactor);
-    } else {
-        width = 20;
-        const scaleFactor = image.cols / width;
-        height = Math.round(image.rows / scaleFactor);
-    }
+  if (height > width) {
+    height = 20;
+    const scaleFactor = image.rows / height;
+    width = Math.round(image.cols / scaleFactor);
+  } else {
+    width = 20;
+    const scaleFactor = image.cols / width;
+    height = Math.round(image.rows / scaleFactor);
+  }
 
-    let newSize = new cv.Size(width, height);
-    cv.resize(image, image, newSize, 0, 0, cv.INTER_AREA)
+  let newSize = new cv.Size(width, height);
+  cv.resize(image, image, newSize, 0, 0, cv.INTER_AREA);
 
-    const LEFT = Math.ceil(4 + (20 - width) / 2);
-    const RIGHT = Math.floor(4 + (20 - width) / 2);
-    const TOP = Math.ceil(4 + (20 - height) / 2);
-    const BOTTOM = Math.floor(4 + (20 - height) / 2);
-    // console.log(`top: ${TOP}, bottom: ${BOTTOM}, left: ${LEFT}, right: ${RIGHT}`);
+  const LEFT = Math.ceil(4 + (20 - width) / 2);
+  const RIGHT = Math.floor(4 + (20 - width) / 2);
+  const TOP = Math.ceil(4 + (20 - height) / 2);
+  const BOTTOM = Math.floor(4 + (20 - height) / 2);
 
-    const BLACK = new cv.Scalar(0, 0, 0, 0);
-    cv.copyMakeBorder(image, image, TOP, BOTTOM, LEFT, RIGHT, cv.BORDER_CONSTANT, BLACK);
+  const BLACK = new cv.Scalar(0, 0, 0, 0);
+  cv.copyMakeBorder(
+    image,
+    image,
+    TOP,
+    BOTTOM,
+    LEFT,
+    RIGHT,
+    cv.BORDER_CONSTANT,
+    BLACK
+  );
 
-    // Centre of Mass
-    cv.findContours(image, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
-    cnt = contours.get(0);
-    const Moments = cv.moments(cnt, false);
+  cv.findContours(
+    image,
+    contours,
+    hierarchy,
+    cv.RETR_CCOMP,
+    cv.CHAIN_APPROX_SIMPLE
+  );
+  cnt = contours.get(0);
+  const Moments = cv.moments(cnt, false);
 
-    const cx = Moments.m10 / Moments.m00;
-    const cy = Moments.m01 / Moments.m00;
-    // console.log(`M00: ${Moments.m00}, cx: ${cx}, cy: ${cy}`);
+  const cx = Moments.m10 / Moments.m00;
+  const cy = Moments.m01 / Moments.m00;
 
-    const X_SHIFT = Math.round(image.cols / 2.0 - cx);
-    const Y_SHIFT = Math.round(image.rows / 2.0 - cy);
+  const X_SHIFT = Math.round(image.cols / 2.0 - cx);
+  const Y_SHIFT = Math.round(image.rows / 2.0 - cy);
 
-    newSize = new cv.Size(image.cols, image.rows);
-    const M = cv.matFromArray(2, 3, cv.CV_64FC1, [1, 0, X_SHIFT, 0, 1, Y_SHIFT]);
-    cv.warpAffine(image, image, M, newSize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, BLACK);
+  newSize = new cv.Size(image.cols, image.rows);
+  const M = cv.matFromArray(2, 3, cv.CV_64FC1, [1, 0, X_SHIFT, 0, 1, Y_SHIFT]);
+  cv.warpAffine(
+    image,
+    image,
+    M,
+    newSize,
+    cv.INTER_LINEAR,
+    cv.BORDER_CONSTANT,
+    BLACK
+  );
 
-    let pixelValues = image.data;
-    // console.log(`pixel values: ${pixelValues}`);
+  let pixelValues = image.data;
 
-    pixelValues = Float32Array.from(pixelValues);
+  pixelValues = Float32Array.from(pixelValues);
 
-    pixelValues = pixelValues.map(function (item) {
-        return item / 255.0;
-    });
+  pixelValues = pixelValues.map(function (item) {
+    return item / 255.0;
+  });
 
-    // console.log(`scaled array: ${pixelValues}`);
+  const X = tf.tensor([pixelValues]);
 
+  const result = model.predict(X);
+  result.print();
+  const output = result.dataSync()[0];
 
-    const X = tf.tensor([pixelValues]);
+  image.delete();
+  contours.delete();
+  cnt.delete();
+  hierarchy.delete();
+  M.delete();
+  X.dispose();
+  result.dispose();
+  print("ans 1", output);
+  return output;
+}
 
-    // console.log(`Shape of Tensor: ${X.shape}`);
-    // console.log(`dtype of Tensor: ${X.dtype}`);
+function predictImage2() {
+  let image = cv.imread(my - canvas2);
+  cv.cvtColor(image, image, cv.COLOR_RGBA2GRAY, 0);
+  cv.threshold(image, image, 175, 255, cv.THRESH_BINARY);
 
-    const result = model.predict(X);
-    result.print();
-    const output = result.dataSync()[0];
+  let contours = new cv.MatVector();
+  let hierarchy = new cv.Mat();
 
+  cv.findContours(
+    image,
+    contours,
+    hierarchy,
+    cv.RETR_CCOMP,
+    cv.CHAIN_APPROX_SIMPLE
+  );
 
-    // console.log(tf.memory());
-    
-    // Testing Only (delete later)
-    // const outputCanvas = document.createElement('CANVAS');
-    // cv.imshow(outputCanvas, image);
-    // document.body.appendChild(outputCanvas);
+  let cnt = contours.get(0);
+  let rect = cv.boundingRect(cnt);
+  image = image.roi(rect);
 
-    // Cleanup
-    image.delete();
-    contours.delete();
-    cnt.delete();
-    hierarchy.delete();
-    M.delete();
-    X.dispose();
-    result.dispose();
+  var height = image.rows;
+  var width = image.cols;
 
-    return output;
+  if (height > width) {
+    height = 20;
+    const scaleFactor = image.rows / height;
+    width = Math.round(image.cols / scaleFactor);
+  } else {
+    width = 20;
+    const scaleFactor = image.cols / width;
+    height = Math.round(image.rows / scaleFactor);
+  }
 
+  let newSize = new cv.Size(width, height);
+  cv.resize(image, image, newSize, 0, 0, cv.INTER_AREA);
+
+  const LEFT = Math.ceil(4 + (20 - width) / 2);
+  const RIGHT = Math.floor(4 + (20 - width) / 2);
+  const TOP = Math.ceil(4 + (20 - height) / 2);
+  const BOTTOM = Math.floor(4 + (20 - height) / 2);
+
+  const BLACK = new cv.Scalar(0, 0, 0, 0);
+  cv.copyMakeBorder(
+    image,
+    image,
+    TOP,
+    BOTTOM,
+    LEFT,
+    RIGHT,
+    cv.BORDER_CONSTANT,
+    BLACK
+  );
+
+  cv.findContours(
+    image,
+    contours,
+    hierarchy,
+    cv.RETR_CCOMP,
+    cv.CHAIN_APPROX_SIMPLE
+  );
+  cnt = contours.get(0);
+  const Moments = cv.moments(cnt, false);
+
+  const cx = Moments.m10 / Moments.m00;
+  const cy = Moments.m01 / Moments.m00;
+
+  const X_SHIFT = Math.round(image.cols / 2.0 - cx);
+  const Y_SHIFT = Math.round(image.rows / 2.0 - cy);
+
+  newSize = new cv.Size(image.cols, image.rows);
+  const M = cv.matFromArray(2, 3, cv.CV_64FC1, [1, 0, X_SHIFT, 0, 1, Y_SHIFT]);
+  cv.warpAffine(
+    image,
+    image,
+    M,
+    newSize,
+    cv.INTER_LINEAR,
+    cv.BORDER_CONSTANT,
+    BLACK
+  );
+
+  let pixelValues = image.data;
+
+  pixelValues = Float32Array.from(pixelValues);
+
+  pixelValues = pixelValues.map(function (item) {
+    return item / 255.0;
+  });
+
+  const X = tf.tensor([pixelValues]);
+
+  const result = model.predict(X);
+  result.print();
+  const output = result.dataSync()[0];
+
+  image.delete();
+  contours.delete();
+  cnt.delete();
+  hierarchy.delete();
+  M.delete();
+  X.dispose();
+  result.dispose();
+  print("ans 2", output);
+  return output;
+}
+
+function predictImage3() {
+  let image = cv.imread(my - canvas3);
+  cv.cvtColor(image, image, cv.COLOR_RGBA2GRAY, 0);
+  cv.threshold(image, image, 175, 255, cv.THRESH_BINARY);
+
+  let contours = new cv.MatVector();
+  let hierarchy = new cv.Mat();
+
+  cv.findContours(
+    image,
+    contours,
+    hierarchy,
+    cv.RETR_CCOMP,
+    cv.CHAIN_APPROX_SIMPLE
+  );
+
+  let cnt = contours.get(0);
+  let rect = cv.boundingRect(cnt);
+  image = image.roi(rect);
+
+  var height = image.rows;
+  var width = image.cols;
+
+  if (height > width) {
+    height = 20;
+    const scaleFactor = image.rows / height;
+    width = Math.round(image.cols / scaleFactor);
+  } else {
+    width = 20;
+    const scaleFactor = image.cols / width;
+    height = Math.round(image.rows / scaleFactor);
+  }
+
+  let newSize = new cv.Size(width, height);
+  cv.resize(image, image, newSize, 0, 0, cv.INTER_AREA);
+
+  const LEFT = Math.ceil(4 + (20 - width) / 2);
+  const RIGHT = Math.floor(4 + (20 - width) / 2);
+  const TOP = Math.ceil(4 + (20 - height) / 2);
+  const BOTTOM = Math.floor(4 + (20 - height) / 2);
+
+  const BLACK = new cv.Scalar(0, 0, 0, 0);
+  cv.copyMakeBorder(
+    image,
+    image,
+    TOP,
+    BOTTOM,
+    LEFT,
+    RIGHT,
+    cv.BORDER_CONSTANT,
+    BLACK
+  );
+
+  cv.findContours(
+    image,
+    contours,
+    hierarchy,
+    cv.RETR_CCOMP,
+    cv.CHAIN_APPROX_SIMPLE
+  );
+  cnt = contours.get(0);
+  const Moments = cv.moments(cnt, false);
+
+  const cx = Moments.m10 / Moments.m00;
+  const cy = Moments.m01 / Moments.m00;
+
+  const X_SHIFT = Math.round(image.cols / 2.0 - cx);
+  const Y_SHIFT = Math.round(image.rows / 2.0 - cy);
+
+  newSize = new cv.Size(image.cols, image.rows);
+  const M = cv.matFromArray(2, 3, cv.CV_64FC1, [1, 0, X_SHIFT, 0, 1, Y_SHIFT]);
+  cv.warpAffine(
+    image,
+    image,
+    M,
+    newSize,
+    cv.INTER_LINEAR,
+    cv.BORDER_CONSTANT,
+    BLACK
+  );
+
+  let pixelValues = image.data;
+
+  pixelValues = Float32Array.from(pixelValues);
+
+  pixelValues = pixelValues.map(function (item) {
+    return item / 255.0;
+  });
+
+  const X = tf.tensor([pixelValues]);
+
+  const result = model.predict(X);
+  result.print();
+  const output = result.dataSync()[0];
+
+  image.delete();
+  contours.delete();
+  cnt.delete();
+  hierarchy.delete();
+  M.delete();
+  X.dispose();
+  result.dispose();
+  print("ans 3", output);
+  return output;
 }
