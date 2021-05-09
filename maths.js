a6cbdc6702959b8765b9aaf57bf660265f153b45
srinivas.kool <31861607+srinivaskool@users.noreply.github.com>
2021-05-09 16:07:34 +0530

    Update maths.js

--- maths.js
+++ maths.js
@@ -1,45 +1,46 @@
-
-var answer;
-var score = 0;
-var backgroundImages = [];
-
+var n1;
+var n2;
+var n3;
 
 function nextQuestion() {
 
-    const n1 = Math.floor(Math.random() * 5);
+    n1 = Math.floor(Math.random() * 5);
     document.getElementById('n1').innerHTML = n1;
 
-    const n2 = Math.floor(Math.random() * 6);
+    n2 = Math.floor(Math.random() * 6);
     document.getElementById('n2').innerHTML = n2;
-    answer = n1 + n2;
+    
+    n3 = Math.floor(Math.random() * 7);
+    document.getElementById('n3').innerHTML = n3;
 }
 
-function checkAnswer() {
+var total_correct = 0;
 
-    const prediction = predictImage();
-    console.log(`Answer: ${answer}, prediction: ${prediction}`);
-
-    if (prediction == answer) {
-        score++;
-        console.log(`Correct! Score: ${score}`);
-        if (score <= 6) {
-            backgroundImages.push(`url('images/background${score}.svg')`);
-            document.body.style.backgroundImage = backgroundImages;
-        } else {
-            alert('Well done! Your maths garden is in full bloom! Want to start again?');
-            score = 0;
-            backgroundImages = [];
-            document.body.style.backgroundImage = backgroundImages;
-        }
-    } else {
-        if (score != 0) { score--; }
-        console.log(`Wrong. Score ${score}`);
-
-        alert('Oops! Check your calculations and try writing the number neater next time!');
-        setTimeout(function () {
-            backgroundImages.pop();
-            document.body.style.backgroundImage = backgroundImages;
-        }, 1000);
+function checkAnswer()
+{
+    total_correct = 0;
+    
+    const prediction1 = predictImage1();
+    if (prediction1 == n1) {
+        total_correct = total_correct +1;
     }
-
+    
+    const prediction2 = predictImage2();
+    if (prediction2 == n2) {
+        total_correct = total_correct +1;
+    }
+    
+    const prediction3 = predictImage3();
+    if (prediction3 == n3) {
+        total_correct = total_correct +1;
+    }
+    
+    if (total_correct == 3) {
+        total_correct = total_correct +1;
+        alert('Verified Succesfully!');
+    }
+    else if(total_correct != 3)
+    {
+        alert('Wrong.Please Check your again!!!' + prediction1 + prediction2 + prediction3);
+    }
 }
